fa806131e5f17e1e48f461de71387545a7d72d16
inwoo <korean5078@gmail.com>
2023-08-25 23:47:38 +0900

    백준 1874번 코드

---
+++ Stack/BOJ1874.java
@@ -0,0 +1,20 @@
+package Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ1874 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < n; i++) {
+			int input = Integer.parseInt(br.readLine());
+			
+		}
+	}
+}
