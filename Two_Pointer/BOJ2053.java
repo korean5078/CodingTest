c3df1b2925a9cbd5c1be52e6d9c46b29a7c716b5
inwoo <korean5078@gmail.com>
2023-08-31 23:47:30 +0900

    백준 2053번 코드

---
+++ Two_Pointer/BOJ2053.java
@@ -0,0 +1,47 @@
+package Two_Pointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2053 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputs = br.readLine().split(" ");
+		int n = Integer.parseInt(inputs[0]), m = Integer.parseInt(inputs[1]);
+		int[] arr = new int[n];
+		int a = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		while (st.hasMoreTokens()) {
+			arr[a++] = Integer.parseInt(st.nextToken());
+		}
+		int cnt = 0;
+		int start = 0, end = 0;
+
+		while (start <= end) {
+			if (end >= arr.length) {
+				break;
+			}
+			int sum = 0;
+			for (int i = start; i <= end; i++) {
+				sum += arr[i];
+			}
+
+			if (sum < m) {
+				end++;
+			} else if (sum > m) {
+				start++;
+				if (start > end) {
+					end++;
+				}
+			} else {
+				cnt++;
+				end++;
+			}
+		}
+
+		System.out.println(cnt);
+		br.close();
+	}
+}
