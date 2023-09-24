86d10290d941ff05b06332c99be86c159cfc7b7c
inwoo <korean5078@gmail.com>
2023-09-24 23:48:10 +0900

    백준 2750번 코드

---
+++ BOJ2750.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BOJ2750 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] > arr[j]) {
+					int tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+
+		for (int i : arr) {
+			System.out.println(i);
+		}
+
+		scanner.close();
+	}
+}
